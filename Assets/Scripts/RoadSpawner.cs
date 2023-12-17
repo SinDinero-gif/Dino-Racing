cad94a793a7c6093162b6cddf59f21ee3fe2729e
SinDinero-gif <samuposadalds@gmail.com>
2023-12-17 18:17:08 -0500

    Update 2 - Prototype

--- Assets/Scripts/RoadSpawner.cs
+++ Assets/Scripts/RoadSpawner.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class RoadSpawner : MonoBehaviour
 {
     public List<GameObject> roads;
-    private float offset = 25f;
+    private float offset = 50f;
 
     // Start is called before the first frame update
     void Start()
