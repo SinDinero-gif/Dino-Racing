7d450bdb1b9957b1839dad6c1f68fac0d754a64d
SinDinero-gif <samuposadalds@gmail.com>
2024-01-25 16:57:38 -0500

    Update 25/01/24

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    public static int coinCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
